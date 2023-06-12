5a38adc40ad65876340db576d4c9d69ba4238521
Andrey Kordik <andreykordik@yandex.ru>
2023-06-12 18:19:07 +0400

    13 sprint

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb').then(() => {
 
 app.use(bodyParser.json());
 
-app.use(authorization);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133',
+  };
+
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('<p>немного html</p>');
--- middlewares/authorization.js
+++
@@ -1,9 +0,0 @@
-const authorization = ((req, res, next) => {
-  req.user = {
-    _id: '6483368ce711963534247a5b',
-  };
-
-  next();
-});
-
-module.exports = authorization;
